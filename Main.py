57c8fa87386e3c1cc9e0fa75279a8550342f9d5c
Anas Assadek <anasassadek@ceroca.cat>
2024-04-17 18:56:11 +0200

    Moviments jugadors

--- Main.py
+++ Main.py
@@ -9,13 +9,16 @@ def main():
     rellotge = pygame.time.Clock()
     gameOver = False
 
-    jugador1 = Jugador(MARGEN_ESCENARIO, ALTO_ESCENARIO // 2 - JUGADOR_ALTO // 2, COLOR_JUGADOR1)
-    jugador2 = Jugador(ANCHO_ESCENARIO - MARGEN_ESCENARIO - JUGADOR_ANCHO, ALTO_ESCENARIO // 2 - JUGADOR_ALTO // 2, COLOR_JUGADOR2)
+    jugador1 = Jugador(MARGEN_ESCENARIO, ALTO_ESCENARIO // 2 - JUGADOR_ALTO // 2, COLOR_JUGADOR1, VELOCIDAD_JUGADOR)
+    jugador2 = Jugador(ANCHO_ESCENARIO - MARGEN_ESCENARIO - JUGADOR_ANCHO, ALTO_ESCENARIO // 2 - JUGADOR_ALTO // 2, COLOR_JUGADOR2, VELOCIDAD_JUGADOR)
 
     while not gameOver:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
+
+        jugador1.mover(pygame.K_w, pygame.K_s)
+        jugador2.mover(pygame.K_UP, pygame.K_DOWN)
 
         finestraJoc.fill(COLOR_FONDO)
         pygame.draw.rect(finestraJoc, VERD, (MARGEN_ESCENARIO, MARGEN_ESCENARIO, ANCHO_ESCENARIO - 2 * MARGEN_ESCENARIO, ALTO_ESCENARIO - 2 * MARGEN_ESCENARIO))
@@ -25,7 +28,7 @@ def main():
 
         pygame.display.update()
         rellotge.tick(30)
-
+        
 if __name__ == "__main__":
     main()
 # 
--- jugador.py
+++ jugador.py
@@ -1,13 +1,21 @@
 import pygame
-from constants import JUGADOR_ANCHO, JUGADOR_ALTO
+from constants import JUGADOR_ANCHO, JUGADOR_ALTO, ALTO_ESCENARIO, MARGEN_ESCENARIO
 
 class Jugador:
-    def __init__(self, posX, posY, color):
+    def __init__(self, posX, posY, color, velocidad):
         self.posX = posX
         self.posY = posY
         self.color = color
         self.ancho = JUGADOR_ANCHO
         self.alto = JUGADOR_ALTO
+        self.velocidad = velocidad  
 
     def dibuja_jugador(self, pantalla):
         pygame.draw.rect(pantalla, self.color, (self.posX, self.posY, self.ancho, self.alto))
+
+    def mover(self, arriba, abajo):
+        teclas = pygame.key.get_pressed()
+        if teclas[arriba] and self.posY - self.velocidad >= MARGEN_ESCENARIO:
+            self.posY -= self.velocidad
+        if teclas[abajo] and self.posY + self.alto + self.velocidad <= ALTO_ESCENARIO - MARGEN_ESCENARIO:
+            self.posY += self.velocidad
